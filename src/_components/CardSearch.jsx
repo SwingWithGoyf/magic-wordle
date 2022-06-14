6fd0deb235628609a0c5a5e8442cb7eadd5d1d64
Jeremy Elgar <elgar.jeremy@gmail.com>
2022-06-14 12:12:57 -0700

    get search showing after 3 chars

--- src/_components/CardSearch.jsx
+++ src/_components/CardSearch.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-// import {
-//   Button, Container, Row, Col, Table,
-// } from 'react-bootstrap';
-import Select from 'react-select';
+import { Dropdown, Form } from 'react-bootstrap';
 
 class CardSearch extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentGuess: ''
+    };
+    
+    this.changeGuess = this.changeGuess.bind(this);
+    this.changeGuessDropdown = this.changeGuessDropdown.bind(this);
+  }
+
+  changeGuess(event) {
+    this.setState({currentGuess: event.target.value});
+  }
+
+  changeGuessDropdown(text) {
+    this.setState({currentGuess: text});
+  }
+  
   componentDidMount() {
     this.props.getCards();
   }
@@ -13,18 +28,42 @@ class CardSearch extends React.Component {
   render() {
     const { cardsdownload } = this.props;
     let cardSearchOptions = [];
-    if (cardsdownload && cardsdownload.card_data) {
-      cardsdownload.card_data.map((item) => (
-        cardSearchOptions.push( {value: item.name, label: item.name})
-      ));
-    }
+    
+    // if (cardsdownload && cardsdownload.card_data) {
+    //   cardsdownload.card_data.map((item) => (
+    //     cardSearchOptions.push( {value: item.name, label: item.name})
+    //   ));
+    // }
 
     return (
       <div className='cardGuess'>
       {cardsdownload && cardsdownload.loading && <em>Loading...</em>}
-      {cardsdownload && cardsdownload.card_data &&
+      {cardsdownload && cardsdownload.card_data && 
         <div>
-          <b>Guess:</b><Select options={cardSearchOptions} />
+          <b>Guess:</b>
+            <Form.Control
+              onChange={this.changeGuess.bind(this)}
+              value={this.state.currentGuess}
+            />
+            {String(this.state.currentGuess).length > 2 
+            ?
+              <Dropdown.Menu show>
+                {cardsdownload.card_data.filter((element) => element.name.toLowerCase().includes(this.state.currentGuess.toLowerCase())).map((item) => 
+                  <Dropdown.Item key={item.name}>{item.name}</Dropdown.Item>
+                )}
+              </Dropdown.Menu>
+            // <Select 
+            //   options={cardSearchOptions}
+            //   onInputChange={this.changeGuessDropdown.bind(this)}
+            //   // onChange TODO: this will fire when they actually pick a guess, we'll need to commit it to the guest table
+            //   defaultInputValue={this.state.currentGuess}
+            //   menuIsOpen={true}
+            //   isSearchable={false}
+            //   controlShouldRenderValue={false}
+            // />
+            : 
+              <div />
+            }
         </div>
       }
       </div>
